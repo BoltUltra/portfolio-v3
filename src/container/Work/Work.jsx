560ab7df1624d9ba3c5ece83aae88377c2d18531
BoltUltra <mayowa.u.sunusi@gmail.com>
2023-05-29 14:25:42 +0100

    '...'

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -101,22 +101,6 @@ const Work = () => {
               <p className="p-text" style={{ marginTop: 10 }}>
                 {work.description}
               </p>
-              <div className="app__flex">
-                <a href={work.projectLink} target="_blank" rel="noreferrer">
-                  <motion.div
-                    className="app__flex"
-                  >
-                    <AiFillEye />
-                  </motion.div>
-                </a>
-                <a href={work.codeLink} target="_blank" rel="noreferrer">
-                  <motion.div
-                    className="app__flex"
-                  >
-                    <AiFillGithub />
-                  </motion.div>
-                </a>
-              </div>
               <div className="app__work-tag app__flex">
                 <p className="p-text">{work.tags[0]}</p>
               </div>
